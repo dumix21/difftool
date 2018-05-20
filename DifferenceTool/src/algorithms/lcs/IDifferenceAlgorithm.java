16de4e677a5d1138fb25a3cd5e2b24836260c906
dumix21 <39221540+dumix21@users.noreply.github.com>
2018-05-20 20:08:07 +0300

    Add files via upload
    Text comparison implementation

---
+++ DifferenceTool/src/algorithms/lcs/IDifferenceAlgorithm.java
@@ -0,0 +1,10 @@
+package algorithms.lcs;
+
+import algorithms.diff.*;
+import java.util.Vector;
+
+public interface IDifferenceAlgorithm {
+	
+	public void populateDifferenceVector(Vector<DiffObj> differences);
+
+}
