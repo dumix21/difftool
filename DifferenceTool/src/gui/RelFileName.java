f7ad65aff50cfb348ec7620a1ea3e6defc2c678d
dumix21 <39221540+dumix21@users.noreply.github.com>
2018-06-02 18:13:33 +0300

    Add files via upload
    Dir comparsion options

--- DifferenceTool/src/gui/RelFileName.java
+++ DifferenceTool/src/gui/RelFileName.java
@@ -1,5 +1,21 @@
 package gui;
 
 public class RelFileName {
-
+	String sName;
+	String sPath;
+	
+	public RelFileName(final String sName, final String sPath) {
+		this.sName=sName;
+		this.sPath=sPath;
+	}
+	
+	public boolean equals(final RelFileName obj) {
+		if(this.sName.equals(obj.sName)) {
+			if(this.sPath.equals(obj.sPath)) {
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
 }
